db6ae368105a33c41ca51b00c23146cfc204fc93
sm3533 <sm3533@g.rit.edu>
2022-01-10 10:04:32 -0500

    Tested
    updated

--- src/java_Intro/HELLOWORLD.java
+++ src/java_Intro/HELLOWORLD.java
@@ -2,6 +2,6 @@ package java_Intro;
 
 public class HELLOWORLD{
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD!");
+        System.out.println("HELLO New WORLD!");
     }
 }
