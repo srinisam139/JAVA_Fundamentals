6823f8e059eb6cbf2cf1cc95b5beb4b80c63c1d8
shrinivassampath Muthupalaniyappan <sm3533@g.rit.edu>
2022-01-23 00:58:55 -0500

    Print Statement is added

--- src/java_Intro/HELLOWORLD.java
+++ src/java_Intro/HELLOWORLD.java
@@ -2,6 +2,8 @@ package java_Intro;
 
 public class HELLOWORLD{
     public static void main(String[] args) {
+        System.out.println("Hello Everyone, How's it going on!");
         System.out.println("HELLO New WORLD!");
+
     }
 }
